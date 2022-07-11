2d7bb0c4b0928d4bd2e4987014535b42bc6f9004
AngelicaLoaiza <angelica.loaiza323@gmail.com>
2022-07-11 13:00:50 -0500

    Llamado a imagenes de cada héroe

--- Ejercicio3/js/superHeroes.js
+++ Ejercicio3/js/superHeroes.js
@@ -3,7 +3,10 @@ window.addEventListener('load', () => {
     const app = Vue.createApp({
         data() {
             return {
-                heroes: []
+                heroes: [],
+                url: '',
+                size: 'standard_large.jpg',
+                img: 'http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg',
             }
         },
         created() {
@@ -11,12 +14,20 @@ window.addEventListener('load', () => {
         },
         methods: {
             listHeroes: async function(){
-                const res = await fetch('https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=117d27d57efac682c626ef9bf2bb98dc&hash=b3f42ecebad378b2e1fa5d85d2fe166d');
+                const img = 'http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg';
+                const res = await fetch('https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=117d27d57efac682c626ef9bf2bb98dc&hash=b3f42ecebad378b2e1fa5d85d2fe166d&limit=100');
                 const data = await res.json();
                 this.heroes = data.data.results;
                 heroes = data.data.results;
                 this.updateLocalStorage();
                 console.log(data.data.results);
+
+                heroes.forEach((item) => {
+                    this.heroes.push(item);
+                    this.url = `${item.thumbnail.path}/${this.size}`
+
+                    console.log(this.url);                    
+                });
             },
             updateLocalStorage: function() {
                 //localStorage.setItem('vue3.heroes'. JSON.stringify(this.heroes)); 
